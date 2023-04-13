4fc8414e4d384a2979b62cd256cae4ba58b2d2d0
EVGENY4510 <lentiay4510z@gmail.com>
2023-04-13 18:41:35 +0300

    hw-10

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+export default function fetchCountries(name) {
+  
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,92 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';   //Notiflix.Notify.failure('Qui timide rogat docet negare');
-                                    // Notiflix.Notify.warning('Memento te hominem esse');
-                                   // Notiflix.Notify.info('Cogito ergo sum');
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  const inputValue = inputEl.value;
+  const name = inputValue.trim();
+  if (name === '') {
+    countryListEl.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(name).then(onResolve).catch(error);
+}
+
+function onResolve(data) {
+  const maxLength = 10;
+  if (data.length > maxLength) {
+    countryListEl.innerHTML = '';
+    countryInfoEl.innerHTML = '';
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  }
+
+  data.forEach(
+    ({
+      name: { official },
+      capital,
+      population,
+      flags: { svg },
+      languages,
+    }) => {
+      const params = {};
+      params.nameCountri = official;
+      params.capitalCountri = capital[0];
+      params.populationCountri = population;
+      params.flag = svg;
+      params.languagesCountri = Object.values(languages).join(',');
+
+      if (data.length === 1) {
+        countryInfoEl.innerHTML = ifOne(params);
+
+        return;
+      }
+      countryListEl.insertAdjacentHTML('beforeend', ifMani(params));
+    }
+  );
+}
+function error() {
+  countryListEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+function ifOne(params) {
+  countryListEl.innerHTML = '';
+  const {
+    nameCountri,
+    capitalCountri,
+    populationCountri,
+    flag,
+    languagesCountri,
+  } = params;
+  return ` <div class="countri-head">
+        <img src="${flag}" alt="flag"  width="40"/>
+        <h1 class="countri-name-one">${nameCountri}</h1>
+      </div>
+      <ul class="card-list">
+        <li class="card-text">Capital:<span class="list-span">${capitalCountri}</span></li>
+        <li class="card-text">Population:<span class="list-span">${populationCountri}</span></li>
+        <li class="card-text">Languages:<span class="list-span">${languagesCountri}</span></li>
+      </ul>`;
+}
+function ifMani(params) {
+  countryInfoEl.innerHTML = '';
+  const { flag, nameCountri } = params;
+  return `<div class="countri-list-wrapper">
+    <li class="countri-flag">
+      <img src="${flag}" alt="flag" width="40" />
+    </li>
+    <li class="countri-name">${nameCountri}</li>
+  </div>`;
+}
